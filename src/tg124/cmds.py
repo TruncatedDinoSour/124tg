a785fe3a53558a944aefde51ce8d9a9265a90f5d
Ari Archer <ari.web.xyz@gmail.com>
2023-10-07 20:39:49 +0300

    add /say

--- src/tg124/cmds.py
+++ src/tg124/cmds.py
@@ -13,3 +13,11 @@ async def ping(msg: Message) -> None:
     """ping the bot"""
 
     await msg.reply_md(f"{msg.user.mention_markdown()} pong")
+
+
+@cmds.new
+async def say(msg: Message, what: str, times: int) -> None:
+    """say something n times"""
+
+    for _ in range(times):
+        await msg.reply_md(what)
